cc13ce1d4cb0721c894d69d04f577f9cabe6d3b4
MaseCodes20 <95643884+MaseCodes20@users.noreply.github.com>
2023-02-14 18:31:19 -0500

    Display boxes for each letter in the correct word

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -4,7 +4,7 @@ import scrambleGame from "../words.json";
 function Game() {
   const [correctWord, setCorrectWord] = useState("");
   const [scrambledWord, setScambledWord] = useState("");
-  const [] = useState();
+  const [wordInput, setWordInput] = useState("");
   const [] = useState();
   const [] = useState();
 
@@ -14,6 +14,17 @@ function Game() {
     return words[randomIndex];
   };
 
+  const newGame = () => {
+    setCorrectWord("");
+    setScambledWord("");
+
+    const word = randomWord();
+    setCorrectWord(word);
+    // console.log(word);
+  };
+
+  console.log(correctWord);
+
   return (
     <div className="border-2 border-black rounded-md min-h-[500px] w-[500px] p-3 shadow-md">
       <div className="text-center mb-3">
@@ -22,8 +33,23 @@ function Game() {
 
       <div>
         {/* Empty letters box */}
+        <div className="flex items-center justify-center gap-2">
+          {correctWord.split("").map((letter, index) => {
+            return (
+              <div
+                key={`${letter}_${indexedDB}`}
+                className="w-10 h-20 bg-gray-300"
+              >
+                {wordInput[index]}
+              </div>
+            );
+          })}
+        </div>
+
         {/* Scrambled Word */}
         {/* word input */}
+
+        <button onClick={newGame}>Click</button>
       </div>
     </div>
   );
